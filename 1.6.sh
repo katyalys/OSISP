d863f73bc5f9084bc06e18e3422a26c7e3d4f34a
katyalys <99620256+katyalys@users.noreply.github.com>
2022-02-27 12:16:45 -0800

    Update 1.6.sh

--- 1.6.sh
+++ 1.6.sh
@@ -1,12 +1,29 @@
 #!/bin/bash
 if [[ $# != 3 ]]; then
-	echo "Check parameters! Enter 3 parameters!" ;
+{
+	echo "Enter right number of parameters"
+	echo "First parameter: file for storing the result"
+	echo "Second parameter: name of catalog"
+	echo "Third parameter: file extension"
+} >&2
 elif [[ $# == 3 ]]; then
-	if [ -f "$1" -a -d "$2" ]; then
-		find $2 -maxdepth 1 -type f -name "*.$3" | sort  >$1
-	else
-		echo "Enter valid parameters!" >&2
+
+	if [ -f "$1" -a -d "$2" ] 
+	then
+		find $2 -maxdepth 1 -type f -name "*.$3" | sort -d >$1
+	fi 
+	
+	if [ ! -d $2 ] 
+	then
+		echo "This directory doesn't exist" >&2	
+		exit 0
 	fi
+	
+	if [ ! -f $1 ] 
+	then
+		echo "This file doesn't exist" >&2	
+		exit 0
+	fi
+	
 fi
 exit 0
-
